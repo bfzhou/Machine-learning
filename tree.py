61b249787076eb05b5f271977123b49865e3d1a7
bfzhou <zhbf15@gmail.com>
2016-09-20 12:26:36 -0500

    2
    2

--- tree.py
+++ tree.py
@@ -9,7 +9,26 @@ import numpy as np
 import scipy
 import io
 import pandas as pd
+import random
+# import tree
 
 #read file into table (parser)
 df = pd.read_table('house-votes-84.data',delimiter=",")
 print(df)
+df1= df.values[1][1]
+print(df1)
+
+#pick 30 data as data set
+datalength = len(df)
+#ascending data
+my_randoms = sorted(random.sample(xrange(datalength), 30))
+df2 = df.loc[my_randoms]
+df3 = df2.values
+df4 = df2.iloc[:, 1]
+df5 = np.bincount(df4.values)
+print(df5)
+
+
+#calculate entropy
+# def entropy(dataset)
+# 	dataset_length = len(dataset)
