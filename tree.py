45970a5c7fc0ff0375cd28d05e044301291942aa
Baofeng Zhou <zhbf15@gmail.com>
2016-09-20 01:28:37 -0500

    read
    read data into table

--- tree.py
+++ tree.py
@@ -7,10 +7,12 @@ Created on Mon Sep 19 16:07:34 2016
 
 import numpy as np
 import scipy
+import io
+import pandas as pd
 
-f = np.loadtxt('house-votes-84.data',dtype={'names': ('class', 'handicapped', 'water', 'adoption', 'physician', 'el-salvador-aid', 'religious', 'anti', 'aid', 'mx', 'immigration', 'synfuels', 'education', 'superfund', 'crime', 'duty', 'export'))
-print(f)
-l = [line.split() for line in f ]
-print (l[0])
-m=np.split(l[0],3)
-print (m)
+#h=open('house-votes-84.data','r')
+df = pd.read_table('house-votes-84.data',delimiter=",")
+
+#datas = np.loadtxt(m,delimiter=",")
+#print (datas)
+print(df)
