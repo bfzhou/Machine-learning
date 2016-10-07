0820b08c83b3c5091b2d4abd1505fa4841afa46a
bfzhou <zhbf15@gmail.com>
2016-10-07 17:20:24 -0500

    h25due

--- hw25/hw25.py
+++ hw25/hw25.py
@@ -11,7 +11,7 @@ def gd(x):
 	return 1/(1+np.exp(-x))
 
 data0 = []
-n=0.1
+n=1
 with open('hw25.csv', newline='') as csvfile:
 	dataset = csv.reader(csvfile, delimiter=',', quotechar='|')
 	for row in dataset:
@@ -33,7 +33,7 @@ w0 = 2*np.random.random((7,4)) - 1
 w1 = 2*np.random.random((5,1)) - 1
 # print(label)
 Error_list= []
-for i in range(10000):
+for i in range(2000):
 	L0 = attr
 	L1 = gd(np.dot(L0,w0))
 	L10 = np.hstack((L1, np.ones((20,1),dtype=attr0.dtype)))
@@ -48,12 +48,15 @@ for i in range(10000):
 	L1_delta = mt(L10) * L1_error
 	w1 += n*L10.T.dot(L2_delta)
 	w0 += n*np.delete(L0.T.dot(L1_delta),-1,-1)
-	if (i%10000==0):
+	if (i%1000==0):
 		print(Error)
 print(w0)
 print(w1)
 print(L2)
 plt.plot(Error_list)
+plt.ylabel('Mean square Error')
+plt.xlabel('Iterations')
+plt.title('Mean square error at learning rate=1')
 plt.show()
 # clf = MLPClassifier(solver='sgd', alpha=1e-5, hidden_layer_sizes=(),max_iter=1000)
 # print(clf.fit(attr, label))
